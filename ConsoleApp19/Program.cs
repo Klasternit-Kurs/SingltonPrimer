84d32cabea1a4d379123c380ac1d9a0f193329a0
Dragan Pepic <d.pepic@hotmail.com>
2020-12-01 19:22:03 +0100

    Add project files.

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SingletonTest st = SingletonTest.Instanciraj();
+			SingletonTest st2 = SingletonTest.Instanciraj();
+			NestoTamo ne = new NestoTamo();
+			if (st == ne.st)
+				Console.WriteLine("Isto :) ");
+			Console.ReadKey();
+		}
+	}
+
+	public class NestoTamo
+	{
+		public SingletonTest st;
+		public NestoTamo()
+		{
+			st = SingletonTest.Instanciraj();
+		}
+	}
+
+	public class SingletonTest
+	{
+		protected SingletonTest() { }
+
+		private static SingletonTest _singl;
+
+		private static object _bzvz = new object();
+
+		public static SingletonTest Instanciraj()
+		{
+			if (_singl != null)
+				lock (_bzvz)
+				{
+					if (_singl != null)
+						_singl = new SingletonTest();
+				}
+
+			return _singl;
+		}
+	}
+}
